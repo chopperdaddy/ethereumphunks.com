017bac7efe7e01114a4190044a52529a83022194
chopper <chopper69daddy@gmail.com>
2023-07-12 14:21:35 -0600

    alchemy provider

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   ethScribeApi: 'https://api.ethscriptions.com/api/',
-  rpcUrl: 'http://geth.dappnode:8545',
+  rpcUrl: 'https://eth-mainnet.g.alchemy.com/v2/19IQKn99eagaaRKD-uSOCE1aYEHLSnmL',
   chainId: 1,
   punkDataAddress: '0x16F5A35647D6F03D5D3da7b35409D65ba03aF3B2',
 };
