694160380ca87b8fdb038e25176906f07871e2a7
chopperdaddy <chopper69daddy@gmail.com>
2025-06-29 22:16:43 -0600

    wallet address refactor

--- marketplace/src/app/directives/wallet-address.directive.ts
+++ marketplace/src/app/directives/wallet-address.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, HostListener, input, effect, untracked, output } from '@angular/core';
 
 import { Web3Service } from '@/services/web3.service';
 import { environment } from '@environments/environment';
@@ -8,49 +8,56 @@ import { environment } from '@environments/environment';
   selector: 'app-address'
 })
 
-export class WalletAddressDirective implements OnChanges {
+export class WalletAddressDirective {
 
-  @Input() address!: string | null;
-  @Input() ens: boolean = true;
-  @Input() clickCopy: boolean = false;
+  address = input<string | null>(null);
+  ens = input<boolean>(true);
+
+  clicked = output<void>();
 
   constructor(
     private el: ElementRef,
     public web3Svc: Web3Service
   ) {
-    this.el.nativeElement.style.cursor = 'pointer';
+    effect(() => {
+      const address = this.address();
+      const ens = this.ens();
+      if (address) {
+        untracked(() => this.setAddress(address, ens));
+      }
+    });
   }
 
   @HostListener('click') onClick(): void {
-    if (this.clickCopy) this.copyToClipboard();
+    this.clicked.emit();
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  async setAddress(address: string, ens: boolean) {
     const el = this.el.nativeElement as HTMLElement;
-    const address = changes.address.currentValue;
 
-    if (address) {
-      if (address === environment.agent.address) {
-        el.innerText = environment.agent.name;
-        return;
-      }
-
-      el.innerText = address.substr(0, 5) + '...' + address.substr(address.length - 5, address.length);
-      if (this.ens) this.getEns(el, address);
+    if (address === environment.agent.address) {
+      el.innerText = environment.agent.name;
       return;
     }
-    el.innerText = '';
+
+    el.innerText = address.slice(0, 5) + '...' + address.slice(-5);
+
+    if (ens) {
+      const ens = await this.getEns(address);
+      if (ens) el.innerText = ens;
+    }
   }
 
-  async getEns(el: HTMLElement, address: string): Promise<void> {
+  async getEns(address: string): Promise<string | null> {
     const ens = await this.web3Svc.getEnsFromAddress(address);
-    if (ens) el.innerText = ens;
+    if (!ens) return null;
+    return ens;
   }
 
   async copyToClipboard(): Promise<void> {
-    if (!navigator.clipboard) return;
-    if (!this.address) return;
-    await navigator.clipboard.writeText(this.address);
+    // if (!navigator.clipboard) return;
+    // if (!this.address) return;
+    // await navigator.clipboard.writeText(this.address);
   }
 
 }
