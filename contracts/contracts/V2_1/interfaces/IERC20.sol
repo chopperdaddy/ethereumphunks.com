5dddc951b10350828096233f1291aa22c33c7247
chopperdaddy <chopper69daddy@gmail.com>
2025-07-30 21:37:32 -0600

    removed unused contracts

--- contracts/contracts/V2_1/interfaces/IERC20.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: PHUNKY
-pragma solidity 0.8.20;
-
-interface IERC20 {
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-}
--- contracts/contracts/V2_1/interfaces/IPoints.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: PHUNKY
-pragma solidity 0.8.20;
-
-interface IPoints {
-    function addPoints(address user, uint256 amount) external;
-}
