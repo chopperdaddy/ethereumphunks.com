f05dd07703026a976abf849ff82ff1f7f825285f
chopperdaddy <chopper69daddy@gmail.com>
2025-08-21 13:12:36 -0600

    removed local ssl serving

--- marketplace/src/environments/environment.ts
+++ marketplace/src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment: Environment = {
   chainId: 11155111,
 
   // rpcHttpProvider: 'http://geth.sepolia-geth.dappnode:8545',
-  // rpcHttpProvider: 'https://eth-sepolia.g.alchemy.com/v2/0FN3yRRyJYmfFlfvjco_d9Y8HaVBIH45',
-  rpcHttpProvider: 'https://ethereum-sepolia-rpc.publicnode.com',
+  rpcHttpProvider: 'https://eth-sepolia.g.alchemy.com/v2/0FN3yRRyJYmfFlfvjco_d9Y8HaVBIH45',
+  // rpcHttpProvider: 'https://ethereum-sepolia-rpc.publicnode.com',
   explorerUrl: 'https://sepolia.etherscan.io',
   externalMarketUrl: 'https://sepolia.ethscriptions.com',
 
