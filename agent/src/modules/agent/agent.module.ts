cb005e946d1400af65677be13f873394f2515444
chopperdaddy <chopper69daddy@gmail.com>
2025-09-07 23:50:02 -0600

    agent stuff. WIP (no prod)

--- agent/src/modules/agent/agent.module.ts
+++ agent/src/modules/agent/agent.module.ts
@@ -2,20 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { KeyGenService } from './services/key-gen.service';
 import { AgentService } from './agent.service';
-import { LangchainService } from './services/langchain.service';
-import { PersonalityDatabaseService } from './services/personality-database.service';
-import { PersonalityService } from './services/personality.service';
-import { PersonalityController } from './personality.controller';
 
 @Module({
   imports: [],
-  controllers: [PersonalityController],
+  controllers: [],
   providers: [
     KeyGenService,
     AgentService,
-    LangchainService,
-    PersonalityDatabaseService,
-    PersonalityService,
   ],
   exports: [],
 })
